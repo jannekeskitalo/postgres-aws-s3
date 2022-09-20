c5375d72c3f05540de72f3279fef7bc20f6a236a
Janne Keskitalo <jke@collectivecrunch.com>
2022-09-21 00:44:28 +0200

    Export test

--- tests/test_install.py
+++ tests/test_install.py
@@ -10,11 +10,21 @@ def connection_parameters():
     with DockerCompose(
         ".", compose_file_name=["docker-compose.yml"], build=True
     ) as compose:
-        host = compose.get_service_host("postgres", 5432)
-        port = compose.get_service_port("postgres", 5432)
-        print(f"Postgres running at: {host}:{port}")
+        db_host = compose.get_service_host("postgres", 5432)
+        db_port = compose.get_service_port("postgres", 5432)
+        s3_port = compose.get_service_port("localstack", 4566)
+        print(f"Postgres running at: {db_host}:{db_port}")
         time.sleep(10)
-        yield {"database": "test", "user": "test", "password": "test", "host": host, "port": port}
+        yield {
+            "postgres": {
+                "database": "test",
+                "user": "test",
+                "password": "test",
+                "host": db_host,
+                "port": db_port,
+            },
+            "s3_endpoint_url": f"http://localstack:{s3_port}",
+        }
 
 
 def install_extension(connection):
@@ -24,9 +34,34 @@ def install_extension(connection):
 
 
 def test_installation(connection_parameters):
-    with psycopg2.connect(**connection_parameters) as connection:
+    with psycopg2.connect(**connection_parameters["postgres"]) as connection:
         install_extension(connection)
         cur = connection.cursor(cursor_factory=NamedTupleCursor)
         cur.execute("select * from pg_extension where extname = 'aws_s3'")
         row = cur.fetchone()
-    assert row.extname == "aws_s3" 
+    assert row.extname == "aws_s3"
+
+
+def test_s3_export(connection_parameters):
+    with psycopg2.connect(**connection_parameters["postgres"]) as connection:
+        cur = connection.cursor(cursor_factory=NamedTupleCursor)
+        cur.execute("create table public.foo as select md5(random()::text) as bar from generate_series(1,100)")
+        cur.execute("select count(*) as cnt from foo")
+        row = cur.fetchone()
+        assert row.cnt == 100
+        export_sql = f"""
+        select * from
+            aws_s3.query_export_to_s3(
+                'select * from public.foo',
+                'test-bucket',
+                'foo.csv',
+                'eu-west-1',
+                'dummy',
+                'dummy',
+                'dummy',
+                options := 'FORMAT CSV, HEADER true',
+                endpoint_url := '{connection_parameters["s3_endpoint_url"]}'
+            )
+        """
+        cur.execute(export_sql)
+    input("Wait...")
